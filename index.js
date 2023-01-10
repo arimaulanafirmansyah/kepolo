6a6a991ba7ff277e0fb4f58140c9058a2a60ca91
arimaulanafirmansyah <arim8062@gmail.com>
2023-01-10 21:12:25 +0700

    first commit

--- index.js
+++ index.js
@@ -8,18 +8,18 @@
 |=======================================|
 */
 var express = require('express'),
-cors = require('cors'),
-secure = require('ssl-express-www');
-const PORT = process.env.PORT || 1 || 2 || 3 
+    cors = require('cors'),
+    secure = require('ssl-express-www');
+const PORT = process.env.PORT || 4000
 /* port edit sesukamu */
 
 var { color } = require('./renz/lib/color.js')
 var mainrouter = require('./routes/main'),
-apirouter = require('./routes/api')
+    apirouter = require('./routes/api')
 
 var app = express()
 app.enable('trust proxy');
-app.set("json spaces",2)
+app.set("json spaces", 2)
 app.use(cors())
 app.use(secure)
 app.use(express.static("./renz/views"))
@@ -30,7 +30,7 @@ app.use('/', mainrouter) // home
 app.use('/api', apirouter) // api
 
 app.listen(PORT, () => {
-console.log(color("Server running on port " + PORT,'green'))
+    console.log(color("Server running on port " + PORT, 'green'))
 })
 
 module.exports = app
